989035d267537071f28fbdd95e89ff4045c9cf5d
Muhammad Bilal <913mbs@gmail.com>
2023-02-02 02:36:36 +0500

    Sample App Next JS Building Blocks

---
+++ pages/posts/first-post.js
@@ -0,0 +1,11 @@
+import Link from "next/link"
+export default function FirstPost() {
+    return (
+        <>
+            <h1>Heading Top</h1>
+            <h2>
+                <Link href="/">Back to Home</Link>
+            </h2>
+        </>
+    )
+}
